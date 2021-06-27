6e95ff26592998bab07218920b47b90d17de75db
mthaku <mayurithakur3001@gmail.com>
2021-06-27 21:09:10 +0530

    proptypes check

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,7 @@
+import PropTypes from "prop-types";
 import "./Card.css";
 
-const Card = ({props}) => {
+const Card = ({ props }) => {
   const {
     id,
     priority,
@@ -12,19 +13,23 @@ const Card = ({props}) => {
     user
   } = props;
 
-
   const colorCode = {
-    "High": "orange",
-    "Low": "skyblue",
-    "Normal": "limegreen",
-    "Urgent": "red"
-  }
+    High: "orange",
+    Low: "skyblue",
+    Normal: "limegreen",
+    Urgent: "red"
+  };
 
   return (
     <div key={id} className="card-container">
       <div className="card-header">
         <div className="card-priority">
-          <div className="priority-tag" style={{backgroundColor: `${colorCode[priority]}`}}>{priority}</div>
+          <div
+            className="priority-tag"
+            style={{ backgroundColor: `${colorCode[priority]}` }}
+          >
+            {priority}
+          </div>
           <div className="card-id">#{id}</div>
         </div>
         <div className="card-created">{created_at}</div>
@@ -33,8 +38,14 @@ const Card = ({props}) => {
       <div className="card-tags">{category}</div>
       <div className="card-footer">
         <div className="card-count">
-          <div ><i className="comment-icon fa fa-comments"></i>{no_of_comments}</div>
-          <div><i className="comment-icon fa fa-paperclip" aria-hidden="true"></i>{attachment_count}</div>
+          <div>
+            <i className="comment-icon fa fa-comments"></i>
+            {no_of_comments}
+          </div>
+          <div>
+            <i className="comment-icon fa fa-paperclip" aria-hidden="true"></i>
+            {attachment_count}
+          </div>
         </div>
         <div>
           <img className="user-thumbnail" src={user} alt="user" />
@@ -42,6 +53,28 @@ const Card = ({props}) => {
       </div>
     </div>
   );
+};
+
+Card.defaultProps = {
+  id: "",
+  priority: "",
+  created_at: "",
+  summary: "",
+  category: "",
+  no_of_comments: "",
+  attachment_count: "",
+  user: ""
+};
+
+Card.propTypes = {
+  id: PropTypes.string.isRequired,
+  priority: PropTypes.string.isRequired,
+  created_at: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  no_of_comments: PropTypes.string.isRequired,
+  attachment_count: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired
 };
 
 export default Card;
--- src/Components/DroppableList/DroppableList.js
+++ src/Components/DroppableList/DroppableList.js
@@ -1,4 +1,6 @@
+import PropTypes from "prop-types";
 import { Droppable, Draggable } from "react-beautiful-dnd";
+
 import Card from "../Card/Card";
 
 const DroppableList = (props) => {
@@ -11,31 +13,42 @@ const DroppableList = (props) => {
           style={getListStyle(snapshot.isDraggingOver)}
         >
           <div className="flex-space-between">
-          <div className="list-header">{title}</div>
-          <div className=" icon-opacity list-header"><i className="fa fa-ellipsis-h fa-lg" aria-hidden="true"></i></div>
+            <div className="list-header">{title}</div>
+            <div className=" icon-opacity list-header">
+              <i className="fa fa-ellipsis-h fa-lg" aria-hidden="true"></i>
+            </div>
           </div>
-          {list.length > 0 && list.map((item, index) => (
-            <Draggable key={item.id} draggableId={item.id} index={index}>
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.draggableProps}
-                  {...provided.dragHandleProps}
-                  style={getItemStyle(
-                    snapshot.isDragging,
-                    provided.draggableProps.style
-                  )}
-                >
-                  <Card props={item} />
-                </div>
-              )}
-            </Draggable>
-          ))}
+          {list.length > 0 &&
+            list.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style
+                    )}
+                  >
+                    <Card props={item} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
           {provided.placeholder}
         </div>
       )}
     </Droppable>
   );
+};
+
+DroppableList.propTypes = {
+  droppableId: PropTypes.string.isRequired,
+  list: PropTypes.array.isRequired,
+  getItemStyle: PropTypes.func.isRequired,
+  getListStyle: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 export default DroppableList;
